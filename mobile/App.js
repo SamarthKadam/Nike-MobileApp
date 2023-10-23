b47fc44cc389b970071229b283b04aba5f688126
SamarthKadam <samarthskadam14@gmail.com>
2023-10-23 17:41:55 +0530

    Initial Setup of Application

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+function App() {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
