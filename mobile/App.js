88b270cc4bc3decb95deb0ea1880292e4ea1ec8a
SamarthKadam <samarthskadam14@gmail.com>
2023-10-24 11:06:40 +0530

    Added Splash Screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+
   return (
     <View>
       <Text>Hello world</Text>
--- mobile/android/app/src/main/java/com/mobile/MainActivity.java
+++ mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -19,6 +20,7 @@ public class MainActivity extends ReactActivity {
 
    @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this); 
     super.onCreate(null);
   }
 
